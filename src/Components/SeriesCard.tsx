23112385cf948fafd98166c1729f2cd14d9e30ac
CalvinPVIII <calvinp003@gmail.com>
2023-06-11 12:03:54 -0700

    adjust responsiveness and layout of Livestream component

--- src/Components/SeriesCard.tsx
+++ src/Components/SeriesCard.tsx
@@ -1,4 +1,3 @@
-import { Stack } from "@chakra-ui/react";
 import "../css/SeriesCard.css";
 
 interface SeriesCardProps {
@@ -15,10 +14,8 @@ export default function SeriesCard(props: SeriesCardProps) {
       <div className="card">
         <img src={props.image} alt={props.name} className="card-image" />
         <div className="card-content">
-          {/* <Stack mt="6" spacing="3"> */}
           <h1 className="card-header">{props.name}</h1>
           {props.type === "series" ? <h2 className="card-body">{props.info} Episodes</h2> : <h2 className="card-body">{props.info} </h2>}
-          {/* </Stack> */}
         </div>
       </div>
     </>
