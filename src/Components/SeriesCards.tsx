1ec9a1b6785ed673c5c4e30568847aadb2f4da3c
CalvinPVIII <calvinp003@gmail.com>
2023-06-19 18:33:54 -0700

    add key to series cards map

--- src/Components/SeriesCards.tsx
+++ src/Components/SeriesCards.tsx
@@ -11,8 +11,8 @@ interface SeriesCardsProps {
 export default function SeriesCards(props: SeriesCardsProps) {
   return (
     <div className="series-cards">
-      {Object.values(props.seriesValues).map((series) => (
-        <div className="series-card">
+      {Object.values(props.seriesValues).map((series, index) => (
+        <div className="series-card" key={index}>
           {props.seriesType === "movie" ? (
             <a href={`/vods/${series.series}/movie/${series.number}`}>
               <SeriesCard name={series.name} info={series.info} shortname={series.info} image={series.image} type={props.seriesType} />
